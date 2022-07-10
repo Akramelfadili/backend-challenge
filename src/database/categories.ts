32e4d15d82cbd94bfe6928d0859d63bebd3946e3
Akramelfadili <fadili261998@gmail.com>
2022-07-10 18:30:38 +0200

    add  : add LEVEL2 implementation

--- src/database/categories.ts
+++ src/database/categories.ts
@@ -1,9 +1,14 @@
+import e from "express"
 import { Categorie } from "src/interfaces/templates"
 import { connectedKnex } from "./knex"
 
 
 export const getAllCategories = () : Promise <Categorie[]> => {
      return connectedKnex("categories").select({id : "id", name : "name" })
+}
+
+export const getAllChildrens = (id : number) :  Promise <Categorie[]>  => {
+     return connectedKnex("categories").where({parent_id : id}).select({id : "id", name : "name"})
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import express from "express"
 import { Request, Response } from 'express';
-import { getAllCategories} from "./database/categories";
+import { getAllCategories, getAllChildrens} from "./database/categories";
 import {} from 'dotenv/config'
+import {Categorie} from "./interfaces/templates"
 
 const app = express()
 const port = process.env.PORT || 3000
 
-
+// should be better to query data with sql directly (better practice) but did it this way cause it's mentionned to leverage the previous challenge
 app.get('/categories', async (request : Request, response : Response) => {
-    const results = await getAllCategories()
-    response.status(200).json(results)
+    const categories = await getAllCategories()
+    const data  : Categorie[] = []
+    for (const category of categories) {
+        const childrens = await getAllChildrens(category.id)
+        data.push(
+            {
+                "id" : category.id,
+                "name" : category.name,
+                "children" : childrens
+            }
+        )
+    }
+    response.status(200).json(data)
 })
 
 app.listen(port, () => console.log(`Server is running on port :${port}`))
